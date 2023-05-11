09ab4847d4a78817d1fb2f0a84c9a0f3347c8efb
admiswalker <admiswalker@gmail.com>
2023-05-11 22:17:22 +0900

    add cdk

--- bin/aws-cdk-private-subnet-ec2-and-s3.ts
+++ bin/aws-cdk-private-subnet-ec2-and-s3.ts
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { AwsCdkPrivateSubnetEc2AndS3Stack } from '../lib/aws-cdk-private-subnet-ec2-and-s3-stack';
+import { AwsCdkTemplateStack } from '../lib/aws-cdk-private-subnet-ec2-and-s3-stack';
 
 const app = new cdk.App();
-new AwsCdkPrivateSubnetEc2AndS3Stack(app, 'AwsCdkPrivateSubnetEc2AndS3Stack');
+
+const prj_name = 'AwsCdkTemplate';
+const env = {
+  account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION
+}
+
+const vpc_stack = new AwsCdkTemplateStack(app, prj_name+'-AwsCdkTemplateStack', {
+  prj_name: prj_name,
+  env: env,
+});
